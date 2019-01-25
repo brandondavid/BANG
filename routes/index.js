f097fdff3d5950f584ca3ff9c21799fb057e4a3b
brandondavid <brandon.david@zoho.com>
2019-01-24 21:13:05 -0800

    Dynamic text

--- routes/index.js
+++ routes/index.js
@@ -10,27 +10,12 @@ router.get('/', (req, res) => {
 	res.render('index', {text: 'This is the dynamic data. Open index.js from the routes directory to see.'})
 })
 
-/*  This route render json data */
-router.get('/json', (req, res) => {
-	res.json({
-		confirmation: 'success',
-		app: process.env.TURBO_APP_ID,
-		data: 'this is a sample json route.'
-	})
-})
-
-/*  This route sends text back as plain text. */
-router.get('/send', (req, res) => {
-	res.send('This is the Send Route')
-})
-
-/*  This route redirects requests to Turbo360. */
-router.get('/redirect', (req, res) => {
-	res.redirect('https://www.turbo360.co/landing')
+router.get('/docs', (req, res) => {
+	res.redirect('/redoc')
 })
 
 router.get('/redoc', (req, res) => {
-	res.render('redoc', {text: 'ReDoc!'})
+	res.render('redoc', {text: 'REDOC!!'})
 })
 
 
