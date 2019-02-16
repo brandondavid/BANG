d3bac569546078edb2f018ff277fd37ca9fb1c94
brandondavid <brandon.david@zoho.com>
2019-02-15 21:17:46 -0800

    Update CompanyController.js

--- controllers/CompanyController.js
+++ controllers/CompanyController.js
@@ -28,7 +28,7 @@ module.exports = {
 
     getPropertyById: (id, params) => {
         return new Promise((resolve, reject) => {
-            Company.findById(id, params)
+            Company.findById(id, params).select('-_id')
             .then(data => {
                 resolve(data)
             })
