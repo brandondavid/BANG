a1c722dc0e6081ded046729de64dbc326b5f7549
brandondavid <brandon.david@zoho.com>
2019-02-18 10:03:57 -0800

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -3,16 +3,14 @@ const vertex = require('vertex360')({site_id: process.env.TURBO_APP_ID})
 const router = vertex.router()
 const controllers = require('../controllers')
 
-//get
+// :resource
 router.get('/:resource', (req, res) => {
 	const resource = req.params.resource
 	const controller = controllers[resource]
-
 	if (controller == null){
 		res.status(404).json({ ERROR: 'Resource Not Found' })
 		return
 	}
-
 	controller.get(req.query)
 	.then(data => {
 		res.status(200).json(data)
@@ -22,16 +20,15 @@ router.get('/:resource', (req, res) => {
 	})
 })
 
-//getById
+
+// :resource/:id
 router.get('/:resource/:id', (req, res) => {
 	const resource = req.params.resource
 	const controller = controllers[resource]
-
 	if (controller == null){
 		res.status(404).json({ ERROR: 'Resource Not Found' })
 		return
 	}
-
 	controller.getById(req.params.id)
 	.then(data => {
 		if (data){
@@ -46,16 +43,63 @@ router.get('/:resource/:id', (req, res) => {
 	})
 })
 
-//getPropertyById
-router.get('/:resource/:id/:property', (req, res) => {
+router.post('/:resource', (req, res) => {
 	const resource = req.params.resource
 	const controller = controllers[resource]
-
 	if (controller == null){
 		res.status(404).json({ ERROR: 'Resource Not Found' })
 		return
 	}
+	controller.post(req.body)
+	.then(data => {
+		res.status(201).json(data)
+	})
+	.catch(err => {
+		res.status(400).json({ ERROR: err.message })
+	})
+})
 
+router.put('/:resource/:id', (req, res) => {
+	const resource = req.params.resource
+	const controller = controllers[resource]
+	if (controller == null){
+		res.status(404).json({ ERROR: 'Resource Not Found' })
+		return
+	}
+	controller.put(req.params.id, req.body)
+	.then(data => {
+		res.status(201).json(data)
+	})
+	.catch(err => {
+		res.status(400).json({ ERROR: err.message })
+	})
+})
+
+router.delete('/:resource/:id', (req, res) => {
+	const resource = req.params.resource
+	const controller = controllers[resource]
+	if (controller == null){
+		res.status(404).json({ ERROR: 'Resource Not Found' })
+		return
+	}
+	controller.delete(req.params.id)
+	.then(data => {
+		res.status(205).json(data)
+	})
+	.catch(err => {
+		res.status(400).json({ ERROR: err.message })
+	})
+})
+
+
+// :resource/:id/:property
+router.get('/:resource/:id/:property', (req, res) => {
+	const resource = req.params.resource
+	const controller = controllers[resource]
+	if (controller == null){
+		res.status(404).json({ ERROR: 'Resource Not Found' })
+		return
+	}
 	controller.getPropertyById(req.params.id, req.params.property)
 	.then(data => {
 		if (data){
@@ -70,57 +114,16 @@ router.get('/:resource/:id/:property', (req, res) => {
 	})
 })
 
-//post
-router.post('/:resource', (req, res) => {
+router.put('/:resource/:id/:property', (req, res) => {
 	const resource = req.params.resource
 	const controller = controllers[resource]
-
 	if (controller == null){
 		res.status(404).json({ ERROR: 'Resource Not Found' })
 		return
 	}
-
-	controller.post(req.body)
+	controller.putPropertyById(req.params.id, req.params.property, req.body)
 	.then(data => {
 		res.status(201).json(data)
-	})
-	.catch(err => {
-		res.status(400).json({ ERROR: err.message })
-	})
-})
-
-//put
-router.put('/:resource/:id', (req, res) => {
-	const resource = req.params.resource
-	const controller = controllers[resource]
-
-	if (controller == null){
-		res.status(404).json({ ERROR: 'Resource Not Found' })
-		return
-	}
-
-	controller.put(req.params.id, req.body)
-	.then(data => {
-		res.status(201).json(data)
-	})
-	.catch(err => {
-		res.status(400).json({ ERROR: err.message })
-	})
-})
-
-//delete
-router.delete('/:resource/:id', (req, res) => {
-	const resource = req.params.resource
-	const controller = controllers[resource]
-
-	if (controller == null){
-		res.status(404).json({ ERROR: 'Resource Not Found' })
-		return
-	}
-
-	controller.delete(req.params.id)
-	.then(data => {
-		res.status(205).json(data)
 	})
 	.catch(err => {
 		res.status(400).json({ ERROR: err.message })
