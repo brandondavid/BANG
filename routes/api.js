780ecf70867dca58358f138c1749ecee07683895
brandondavid <brandon.david@zoho.com>
2019-02-18 15:34:11 -0800

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -121,7 +121,7 @@ router.put('/:resource/:id/:property', (req, res) => {
 		res.status(404).json({ ERROR: 'Resource Not Found' })
 		return
 	}
-	controller.putPropertyById(req.params.id, {$set: {req.params.property: req.body}})
+	controller.putPropertyById(req.params.id, {req.params.property: req.body})
 	.then(data => {
 		res.status(201).json(data)
 	})
